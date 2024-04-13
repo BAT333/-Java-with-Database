32154c1d1aaae57dcc1d85ab83bc5d5d82a29a1d
RafaelBat-alves <152424427+RafaelBat-alves@users.noreply.github.com>
2024-04-13 19:08:02 -0300

    JDBC JAVA

---
+++ JavaJDBC/src/main/java/org/example/jdbc/Main.java
@@ -0,0 +1,19 @@
+package org.example.jdbc;
+
+import org.example.jdbc.Client.Client;
+import org.example.jdbc.Client.Database.ClientDao;
+import org.example.jdbc.Client.Database.ConnectionFactory;
+import org.example.jdbc.Client.Service.ClientService;
+
+public class Main {
+    public static void main(String[] args) {
+        Client client = new Client("ROBERTO","50","RUA JOAQUIM NOBERTO");
+        ClientService service = new ClientService();
+        service.saveClient(client);
+       // service.listClient().forEach(System.out::println);
+        service.UpdateClient(client,4);
+        service.listClient().forEach(System.out::println);
+        //service.deleteClient(4);
+
+    }
+}
